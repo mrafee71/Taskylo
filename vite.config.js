3c4430185098f597ef77f14fdab896673f8ff9bf
mrafee71 <muhammadrafeequeph@gmail.com>
2024-12-25 19:38:04 +0530

    Second

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import reactRefresh from '@vitejs/plugin-react-refresh';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [reactRefresh()],
+  base: '/Taskylo/', // Replace 'todo-app' with your repo name
+});
+
